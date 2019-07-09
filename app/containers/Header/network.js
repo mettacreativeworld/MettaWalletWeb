baf55f138e3d199e70349b79372d3edbc0f2be83
sriharikapu <srihari@blockchaintmhub.io>
2019-07-09 16:58:35 +0800

    network

--- app/containers/Header/network.js
+++ app/containers/Header/network.js
@@ -2,8 +2,8 @@
 const Network = {
   Offline: { rpc: 'offline', tx_explorer: null },
   'Local RPC': { rpc: 'http://127.0.0.1:8545', tx_explorer: null },
-  'Ropsten Testnet': { rpc: 'https://ropsten.infura.io/GjiCzFxpQAUkPtDUpKEP', tx_explorer: 'https://ropsten.etherscan.io/tx/' },
-  'Main Net': { rpc: 'https://mainnet.infura.io/GjiCzFxpQAUkPtDUpKEP', tx_explorer: 'https://etherscan.io/tx/' },
+  'Ropsten Testnet': { rpc: 'https://ropsten.infura.io/v3/8835232ce9b445b5a441ed0efa7e9395', tx_explorer: 'https://ropsten.etherscan.io/tx/' },
+  'Main Net': { rpc: 'https://mainnet.infura.io/v3/8835232ce9b445b5a441ed0efa7e9395', tx_explorer: 'https://etherscan.io/tx/' },
 };
 
 module.exports = Network;
